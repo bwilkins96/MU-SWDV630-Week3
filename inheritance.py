db728a34fa8ba3b7549df991bd3da6c7c83c094a
bwilkins96 <ben.jr.speed@gmail.com>
2023-05-26 10:41:46 -0400

    person classes setup

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+# SWDV 630 - Object-Oriented Software Architecture
+# Person superclass and 3 subclasses for a hotel management system
+
+class Person:
+    pass
+
+class Guest(Person):
+    pass
+
+class Employee(Person):
+    pass
